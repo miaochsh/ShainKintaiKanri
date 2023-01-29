3d704c42d62c4c3ef881676ef3cefb6c13c771d3
miaochsh <miaochsh@outlook.com>
2023-01-29 14:00:34 +0900

    テスト

--- ShainKintaiKanri/src/main/java/jp/co/bss/kintai/controller/KintaiController.java
+++ ShainKintaiKanri/src/main/java/jp/co/bss/kintai/controller/KintaiController.java
@@ -1,9 +1,14 @@
 package jp.co.bss.kintai.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @author miaoc
  *
  */
+@Controller
+@RequestMapping("/items")
 public class KintaiController {
 
 }
