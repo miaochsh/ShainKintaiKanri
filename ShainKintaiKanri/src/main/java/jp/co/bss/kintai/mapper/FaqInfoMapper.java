2e834cf46be25aa47d3ed7e32c54c1fa6403d24d
Liuyuchao93 <141922523+Liuyuchao93@users.noreply.github.com>
2023-09-15 11:30:08 +0900

    FaqInfoMapper

--- ShainKintaiKanri/src/main/java/jp/co/bss/kintai/mapper/FaqInfoMapper.java
+++ ShainKintaiKanri/src/main/java/jp/co/bss/kintai/mapper/FaqInfoMapper.java
@@ -1,16 +1,16 @@
 /**
- * FAQ
+ * 
  */
 package jp.co.bss.kintai.mapper;
+
+import java.util.List;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
 import jp.co.bss.kintai.dao.FaqInfo;
-import java.util.List;
 
 /**
  * @author lyc
@@ -18,17 +18,14 @@ import java.util.List;
  */
 @Mapper
 public interface FaqInfoMapper {
-	
-	@Select("SELECT * FROM public.faq WHERE name = #{name}")
 	FaqInfo getFaqInfoByUserName(String name);
+	
+	List<FaqInfo> getFaqListInfoByUserName();
 
-	@Select("SELECT * FROM public.faq")
-	List<FaqInfo> getFaqInfoListByUserName();
-	
 	@Insert("INSERT INTO faq (name) VALUES (#{name})")
-    void insertFAQ(@Param("name") String name);
-	
+	void insertFAQ(@Param("name") String name);
+
 	@Update("UPDATE faq SET question1 = #{question1}, answer1 = #{answer1}, question2 = #{question2}, answer2 = #{answer2}, question3 = #{question3}, answer3 = #{answer3},create_date = #{create_date} WHERE name = #{name}")
-    void updateFAQ(FaqInfo faq);
+	void updateFAQ(FaqInfo faq);
 
 }
