6d900a68085585a257ef67668b05438a22726681
Liuyuchao93 <141922523+Liuyuchao93@users.noreply.github.com>
2023-09-27 17:55:31 +0900

    faq SQLファイル更新

--- SQL/faq.sql
+++ SQL/faq.sql
@@ -1,14 +1,13 @@
 create table public.faq (
   id serial not null
-  , name character varying(255)
-  , question1 character varying(255)
-  , answer1 character varying(255)
-  , question2 character varying(255)
-  , answer2 character varying(255)
-  , question3 character varying(255)
-  , answer3 character varying(255)
+  , claimer character varying(255)
+  , question character varying(255)
+  , reponder character varying(255)
+  , answer character varying(255)
+  , answerflg character varying(1)
   , update_date timestamp(6) without time zone
   , create_date timestamp(6) without time zone
   , delete_date timestamp(6) without time zone
+  , blob_data bytea
   , primary key (id)
 );
