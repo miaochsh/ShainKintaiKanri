9525e5cfd7e1ec44ab5c05048644714ea6382f7c
BSS_Onuma <develop05@bss-j.co.jp>
2023-10-13 14:57:50 +0900

    社内FAQの画面遷移を行いました

--- ShainKintaiKanri/src/main/java/jp/co/bss/kintai/controller/FaqController.java
+++ ShainKintaiKanri/src/main/java/jp/co/bss/kintai/controller/FaqController.java
@@ -1,16 +1,10 @@
 package jp.co.bss.kintai.controller;
 
-import java.util.List;
-
 import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import jp.co.bss.kintai.model.FaqInfo;
-import jp.co.bss.kintai.service.FaqService;
 
 /**
  * FaqController
@@ -20,17 +14,19 @@ public class FaqController {
 	/*
 	 * Faq情報 Service
 	 */
-	@Autowired
-	private FaqService faqService;
+	/*
+	 * @Autowired private FaqService faqService;
+	 */
 
 	/*
 	 * Faq情報一覧画面を表示
 	 */
 	@GetMapping("/faq")
-	public String faq(HttpSession session, Model model) {
-		List<FaqInfo> data = faqService.getFaqInfoList();
-		model.addAttribute("faqs", data);
+	public String showFaq(HttpSession session, Model model) {
+		/*
+		 * List<FaqInfo> data = faqService.getFaqInfoList(); model.addAttribute("faqs",
+		 * data);
+		 */
 		return "faq";
-	}	
-	
+	}
 }
