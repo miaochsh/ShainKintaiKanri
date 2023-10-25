c28e1e13673b88b8ddce3f47ba48a341a562f8ab
develop05 <147013917+BSS-OnumaTakuto@users.noreply.github.com>
2023-10-25 11:54:23 +0900

    Serviceクラスが漏れていたので追加しました。

--- ShainKintaiKanri/src/main/java/jp/co/bss/kintai/service/NotificationsService.java
+++ ShainKintaiKanri/src/main/java/jp/co/bss/kintai/service/NotificationsService.java
@@ -19,6 +19,10 @@ public class NotificationsService {
 		return data;
 	}
 	
+	public List<NotificationsInfo> getImportantNotifications() {
+	    return notificationsInfoMapper.getImportantNotifications();
+	}
+	
 	@Transactional
 	public void insertNotifications1(NotificationsInfo notifications) {
 		notificationsInfoMapper.insertNotifications1(notifications);
